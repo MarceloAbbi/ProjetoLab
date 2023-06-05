f833126f2ff777e9306613437c82739532a8d77f
Marcelo Abbi <marcelo.abbi@outlook.com>
2023-06-05 14:43:17 -0300

    atualizacoes

--- unicoding/js/plano.js
+++ unicoding/js/plano.js
@@ -27,7 +27,7 @@ class todoList{
         this.input = document.createElement('input');
         this.input.classList.add("comment");
         this.button = document.createElement('button');
-        this.button.innerText = 'Add';
+        this.button.innerText = 'Adicionar';
         this.button.classList.add("btn-save");
         this.button.id = "to-do-list-button";
         this.div = document.createElement('div');
@@ -111,8 +111,8 @@ class Card{
         this.commentsInput.className = "commentsInput comment";
         this.commentsButton.className = "commentsButton btn-save";
 
-        this.commentsButton.innerText = "Add";
-        this.commentsInput.placeholder = "Write a comment...";
+        this.commentsButton.innerText = "Adicionar";
+        this.commentsInput.placeholder = "Escreva um comentário...";
 
         this.menuContainer.addEventListener('click', (e)=>{
             console.log(e.target);
@@ -265,6 +265,46 @@ let todoList3 = new todoList(root);
 
 
 
-todoList1.input.value = "asdasds";
+todoList1.input.value = "Estudo";
 todoList1.addToDo();
 
+window.addEventListener('DOMContentLoaded', event => {
+
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    navbarShrink();
+
+    document.addEventListener('scroll', navbarShrink);
+
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
